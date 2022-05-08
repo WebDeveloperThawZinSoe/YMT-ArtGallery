3aa7bbcbc11fb2b5f26d4bf070c31c42b4d52f95
Thaw <63851168+WebDeveloperThawZinSoe@users.noreply.github.com>
2022-05-08 19:52:20 +0630

    set up

---
+++ base/head.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>YMT-ArtGallery</title>
+  <meta charset="utf-8">
+  <meta name="description" content="YMT Art Gallery Store">
+  <meta name="keywords" content="YMT, YMT-Art Gallery, Gallery,Store">
+  <meta name="author" content="Thaw Zin Soe">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<?php
+  include_once "database.php";
+?>
+
+<!-- 
+
+Contact Me If You Want Website
+
+Developed By Thaw Zin Soe 
+Phone Number +959403077739
+Email thawzinsoe.dev@gmail.com 
+GitHub https://github.com/WebDeveloperThawZinSoe
+WebsiteLink https://webdeveloperthawzinsoe.github.io/Thaw-Zin-Soe/
+LinkedIn https://www.linkedin.com/in/thaw-zin-soe-0b92b61a5/
+
+-->
